605201c92675954e1065b282eb440ceb8ce73316
Kyle <kqsavell@wpi.edu>
2018-02-16 14:49:11 -0500

    Starting to read input

--- p4.c
+++ p4.c
@@ -2,9 +2,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Instructions?: \n");
+    int pid = 0; // Process ID
+    int inst_type = 0; // Instruction type
+    int v_addr = 0; // Virtual address
+    int input = 0; // Value
+
+    // Read stdin
+    printf("Instructions?: ");
+    if (argc >= 2)
+    {
+        pid = *argv[1];
+    }
+    if (argc >= 3)
+    {
+        if (strncmp(argv[2], "load", sizeof(argv[2])))
+        {
+            inst_type = 1;
+        }
+    }
+    if (argc >= 4)
+    {
+        v_addr = *argv[3];
+    }
+    if (argc >= 5)
+    {
+        input = *argv[4];
+    }
+    printf("%d, %d, %d, %d\n", pid, inst_type, v_addr, input);
+
     return 0;
 }
